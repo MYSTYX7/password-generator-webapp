19adcb477ed6b1d1550ab6598ec3a9d818ad26b1
Abhinav Singh Chauhan <cool.abhi@outlook.com>
2024-01-20 16:58:59 +0530

    feat: Character Slider completed

--- src/Components/Generator.jsx
+++ src/Components/Generator.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+import CharLen from "./GeneratorComponents/CharLen";
+
 const Generator = ({ onGenerate }) => {
-  // Password Generation Logic
-  const generatePassword = () => {
-    const generatedPassword = Math.random();
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const handleSliderChange = (value) => {
+    setSliderValue(value);
+    generatePassword(value);
+  };
+
+  const generatePassword = (value) => {
+    const characters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-[]{}|;~:.<>?";
+    let generatedPassword = "";
+
+    for (let i = 0; i < value; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      generatedPassword += characters.charAt(randomIndex);
+    }
+
     onGenerate(generatedPassword);
   };
 
   return (
     <div className="bg-darkgray-c items-center p-5">
+      <CharLen
+        currentValue={sliderValue}
+        onSliderChange={(value) => handleSliderChange(value)}
+      />
       <button
         className="bg-green-c px-auto py-4 w-full font-bold"
         onClick={generatePassword}
---
+++ src/Components/GeneratorComponents/CharLen.jsx
@@ -0,0 +1,32 @@
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import { useState } from "react";
+
+const CharLen = ({ currentValue, onSliderChange }) => {
+  return (
+    <div className="flex flex-col pb-5">
+      <div className="flex justify-between items-center text-white-c">
+        <p className="text-sm">Character Length</p>
+        <p className="text-2xl text-green-c">{currentValue || 0}</p>
+      </div>
+      <div className="pt-3">
+        <Slider
+          onChange={onSliderChange}
+          max={20}
+          trackStyle={{ backgroundColor: "#a5feaf", height: 6 }}
+          railStyle={{ backgroundColor: "#030712", height: 6 }}
+          handleStyle={{
+            borderColor: "white",
+            height: 20,
+            width: 20,
+            marginTop: -6.25,
+            backgroundColor: "white",
+            opacity: 1,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CharLen;
